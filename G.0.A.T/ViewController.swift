c5bd6127e3b404baba71fad7df3cc94828b3ec0b
Janice Lai <janicelai@janices-macbook-pro.local>
2018-09-09 20:19:01 -0700

    Initial Commit

---
+++ G.0.A.T/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  G.0.A.T
+//
+//  Created by Janice Lai on 9/9/18.
+//  Copyright © 2018 Janice Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
